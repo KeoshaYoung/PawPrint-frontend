384b22a15ecd4a3f756a46ca80495b4d4d08920d
keoshayoung <keoshayoung@email.com>
2022-09-01 13:52:18 -0400

    installed axios and created index function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,5 @@
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
@@ -6,14 +7,28 @@ export default {
       youthChallenges: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexYouthChallenges();
+  },
+  methods: {
+    indexYouthChallenges: function () {
+      axios.get("http://localhost:3000/youth_challenges.json").then((response) => {
+        this.youthChallenges = response.data;
+        console.log("all Youth Challenges:", this.youthChallenges);
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <h2>All Youth Challenges</h2>
+    <div v-for="youthChallenge in youthChallenges" v-bind:key="youthChallenge.id">
+      <h4>{{ youthChallenge.name }}</h4>
+      <p>{{ youthChallenge.address }}</p>
+    </div>
   </div>
 </template>
 
